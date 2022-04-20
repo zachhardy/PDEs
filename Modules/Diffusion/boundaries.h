1402d5438606baed7917bbaca9d24f94034fba57
Zachary Hardy <zhardy1115@gmail.com>
2022-04-20 14:55:01 -0600

    Cleaned up boundaries.

--- Modules/Diffusion/boundaries.h
+++ Modules/Diffusion/boundaries.h
@@ -30,6 +30,7 @@ enum class BoundaryType
 class diffusion::Boundary
 {
 public:
+  /// An identifier for the boundary type.
   const BoundaryType type;
 
 public:
@@ -47,16 +48,13 @@ public:
 class diffusion::DirichletBoundary : public Boundary
 {
 public:
-  double value = 0.0;
+  double value = 0.0; ///< The boundary value.
 
 public:
   /// Construct a homogeneous Dirichlet boundary.
   DirichletBoundary() : Boundary(BoundaryType::DIRICHLET) {}
 
-  /**
-   * \brief Construct an inhomogeneous Dirichlet boundary.
-   * \param boundary_value The boundary value.
-   */
+   /// Construct a Dirichlet boundary.
   explicit DirichletBoundary(double boundary_value)
     : Boundary(BoundaryType::DIRICHLET), value(boundary_value)
   {}
@@ -67,22 +65,18 @@ public:
 /**
  * \brief Derived Boundary class for Neumann boundaries.
  *
- * These boundaries are given by
- * \f$ \partial_{\hat{n}_b} u = f^n \f$.
+ * These boundaries are given by \f$ \partial_{\hat{n}_b} u = f^n \f$.
  */
 class diffusion::NeumannBoundary : public Boundary
 {
 public:
-  double value = 0.0;
+  double value = 0.0; ///< The boundary derivative.
 
 public:
   /// Construct a reflective boundary.
   NeumannBoundary() : Boundary(BoundaryType::NEUMANN) {}
 
-  /**
-   * \brief Construct a mon-reflective boundary.
-   * \param boundary_value The boundary value.
-   */
+  /// Construct a Neumann boundary.
   explicit NeumannBoundary(double boundary_value)
     : Boundary(BoundaryType::NEUMANN), value(boundary_value)
   {}
@@ -98,9 +92,9 @@ public:
 class diffusion::RobinBoundary : public Boundary
 {
 public:
-    double a = 0.25; ///< The scaling factor for the solution.
-    double b = 0.5;  ///< The scaling factor for the solution gradient.
-    double f = 0.0;  ///< The source term.
+    double a = 0.25; ///< The scaling factor for the boundary value.
+    double b = 0.5;  ///< The scaling factor for the boundary derivative.
+    double f = 0.0;  ///< The boundary source term.
 
 public:
   /// Contruct a vacuum boundary.
@@ -116,9 +110,9 @@ public:
 
   /**
    * \brief Construct a fully specified Robin boundary
-   * \param a_value The scaling factor for the solution.
-   * \param b_value The scaling factor for the solution gradient.
-   * \param f_value The source term.
+   * \param a_value The scaling factor for the boundary value.
+   * \param b_value The scaling factor for the boundary derivative.
+   * \param f_value The boundary source term.
    */
   explicit RobinBoundary(double a_value, double b_value, double f_value)
       : Boundary(BoundaryType::ROBIN), a(a_value), b(b_value), f(f_value)
