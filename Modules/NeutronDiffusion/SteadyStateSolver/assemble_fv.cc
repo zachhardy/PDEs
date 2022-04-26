d39c0d5a82eb5cab4043ccdc93667f0621de2196
Zachary Hardy <zhardy1115@gmail.com>
2022-04-26 10:19:56 -0600

    Updated assembly to reflect differnt solve types.

--- Modules/NeutronDiffusion/SteadyStateSolver/assemble_fv.cc
+++ Modules/NeutronDiffusion/SteadyStateSolver/assemble_fv.cc
@@ -1,111 +1,122 @@
 #include "steadystate_solver.h"
 
-/**
- * \brief Assemble the full multigroup diffusion operator using the finite
- *        volume method.
- *
- * This routine first clears the \p system_matrix. It then assembles the full
- * multigroup operator, including the group-wise total interaction, diffusion,
- * and boundary terms as well as the cross-group scattering and fission term.
- */
 void neutron_diffusion::SteadyStateSolver::assemble_fv_matrix()
 {
-  system_matrix *= 0.0; // Clear the matrix
+  math::Matrix& A = system_matrix;
+  A *= 0.0;
 
-  // Build the system cell by cell
+  //================================================== Loop over cells
   for (const auto& cell : mesh->cells)
   {
     double volume = cell->volume;
     const auto& xs = material_xs[matid_to_xs_map[cell->material_id]];
-    const size_t i = cell->id * n_groups;
+    size_t i = cell->id * n_groups;
 
-    // Assemble group-wise cell terms
+    // Total interaction term
     for (size_t g = 0; g < n_groups; ++g)
+      A[i + g][i + g] += xs->sigma_t[g] * volume;
+
+    //================================================== Group transfers
+    if (solution_method == SolutionMethod::DIRECT)
     {
-      // Total interaction term
-      system_matrix[i + g][i + g] += xs->sigma_t[g] * volume;
-
-      // Assemble scattering term
-      for (size_t gp = 0; gp < n_groups; ++gp)
-        system_matrix[i + g][i + gp] -=
-            xs->transfer_matrices[0][g][gp] * volume;
-
-      // Assemble fission term
-      if (xs->is_fissile)
+      for (size_t g = 0; g < n_groups; ++g)
       {
-        double chi = (not use_precursors)? xs->chi[g] : xs->chi_prompt[g];
+        // Scattering term
+        for (size_t gp = 0; gp < n_groups; ++gp)
+          A[i + g][i + gp] -= xs->transfer_matrices[0][g][gp] * volume;
 
-        for (size_t gp = 0; g < n_groups; ++gp)
+        // Fission term
+        if (xs->is_fissile)
         {
-          double nu_sig_f = (not use_precursors)? xs->nu_sigma_f[gp] :
-                                                  xs->nu_prompt_sigma_f[gp];
-          system_matrix[i + g][i + gp] -= chi * nu_sig_f * volume;
-        }
-      }//if fissile
-    }//for group
+          // Total fission term
+          if (not use_precursors)
+          {
+            double chi = xs->chi[g];
+            for (size_t gp = 0; gp < n_groups; ++gp)
+              A[i + g][i + gp] -= chi * xs->nu_sigma_f[gp] * volume;
+          }
 
-    // Assemble group-wise face terms
+          // Prompt + delayed term
+          else
+          {
+            double chi_p = xs->chi_prompt[g];
+            for (size_t gp = 0; gp < n_groups; ++gp)
+              A[i + g][i + gp] -= chi_p * xs->nu_prompt_sigma_f[gp] * volume;
+
+            for (size_t j = 0; j < xs->n_precursors; ++j)
+            {
+              double chi_d = xs->chi_delayed[g][j];
+              double gamma = xs->precursor_yield[j];
+              for (size_t gp = 0; gp < n_groups; ++gp)
+                A[i + g][i + gp] -=
+                    chi_d * gamma * xs->nu_delayed_sigma_f[gp] * volume;
+            }
+          }
+        }//if fissile
+      }//for group
+    }//if off-diagonal terms
+
+    //================================================== Loop over faces
     for (const auto& face : cell->faces)
     {
-      // Diffusion across interior faces
+      //================================================== Interior faces
+      // Diffusion term
       if (face.has_neighbor)
       {
+        // Get neighbor cell
         const auto& nbr_cell = mesh->cells[face.neighbor_id];
-        const size_t j = nbr_cell->id * n_groups;
-
-        const int nbr_xs_id = matid_to_xs_map[nbr_cell->material_id];
-        const auto& nbr_xs = material_xs[nbr_xs_id];
+        int nbr_mat_id = nbr_cell->material_id;
+        const auto& nbr_xs = material_xs[nbr_mat_id];
+        size_t j = nbr_cell->id * n_groups;
 
         // Geometric quantities
-        double d_pn = cell->centroid.distance(nbr_cell->centroid);
         double d_pf = cell->centroid.distance(face.centroid);
-        double w = d_pf / d_pn; // Weighting factor for harmonic means
-
-        // Diffusion coefficients
-        const auto& D = xs->diffusion_coeff;
-        const auto& D_nbr = nbr_xs->diffusion_coeff;
+        double d_pn = cell->centroid.distance(nbr_cell->centroid);
+        double w = d_pf / d_pn; // weighting factor for harmonic mean
 
         for (size_t g = 0; g < n_groups; ++g)
         {
-          // Effective diffusion coefficient on face
-          double D_eff = 1.0 / (w/D[g] + (1.0 - w)/D_nbr[g]);
+          // Diffusion coefficients
+          double D = xs->diffusion_coeff[g];
+          double D_nbr = nbr_xs->diffusion_coeff[g];
+          double D_eff = 1.0 / (w/D + (1.0 - w)/D_nbr);
 
-          system_matrix[i + g][i + g] += D_eff/d_pn * face.area;
-          system_matrix[i + g][j + g] -= D_eff/d_pn * face.area;
-        }//for group
+          A[i + g][i + g] += D_eff / d_pn * face.area;
+          A[i + g][j +g] -= D_eff / d_pn * face.area;
+        }
       }//if interior face
 
-      // Assemble group-wise boundary terms
+      //================================================== Boundary faces
       else
       {
-        const auto bndry_id = face.neighbor_id;
-        const auto bndry_type = boundary_info[bndry_id].first;
+        const size_t bndry_id = face.neighbor_id;
+        const auto& bndry_type = boundary_info[bndry_id].first;
 
-        // Handle Dirichlet boundaries
+        // Dirichlet boundary term
         if (bndry_type == BoundaryType::ZERO_FLUX or
             bndry_type == BoundaryType::DIRICHLET)
         {
           double d_pf = cell->centroid.distance(face.centroid);
-          const auto& D = xs->diffusion_coeff;
-
           for (size_t g = 0; g < n_groups; ++g)
-            system_matrix[i + g][i + g] += D[g]/d_pf * face.area;
+          {
+            double D = xs->diffusion_coeff[g];
+            A[i + g][i + g] += D / d_pf * face.area;
+          }
         }
 
-        // Handle Robin boundaries
-        if (bndry_type == BoundaryType::VACUUM or
-            bndry_type == BoundaryType::MARSHAK or
-            bndry_type == BoundaryType::ROBIN)
+        // Robin boundary term
+        else if (bndry_type == BoundaryType::VACUUM or
+                 bndry_type == BoundaryType::MARSHAK or
+                 bndry_type == BoundaryType::ROBIN)
         {
           double d_pf = cell->centroid.distance(face.centroid);
-          const auto& D = xs->diffusion_coeff;
-
           for (size_t g = 0; g < n_groups; ++g)
           {
             const auto& bndry = boundaries[bndry_id][g];
             const auto bc = std::static_pointer_cast<RobinBoundary>(bndry);
-            system_matrix[i + g][i + g] +=
-                bc->a*D[g]/(bc->b*D[g] + bc->a*d_pf) * face.area;
+
+            double D = xs->diffusion_coeff[g];
+            A[i + g][i + g] += bc->a*D/(bc->b*D + bc->a*d_pf) * face.area;
           }
         }
       }//if boundary face
@@ -115,87 +126,111 @@ void neutron_diffusion::SteadyStateSolver::assemble_fv_matrix()
 
 //######################################################################
 
-/**
- * \brief Assemble the full multigroup right-hand side vector using the
- *        finite volume method.
- *
- * This routine first clears the \p system_rhs. It then assembles the full
- * multigroup right-hand side vector, including the multigroup inhomogeneous
- * source and boundary sources.
- */
-void neutron_diffusion::SteadyStateSolver::assemble_fv_rhs()
+void neutron_diffusion::SteadyStateSolver::set_fv_source()
 {
-  system_rhs *= 0.0; // Clear the rhs
+  math::Vector& b = system_rhs;
+  b *= 0.0;
 
-  // Assemble cell by cell
+  //================================================== Loop over cells
   for (const auto& cell : mesh->cells)
   {
     double volume = cell->volume;
-    const size_t i = cell->id * n_groups;
+    const auto& xs = material_xs[matid_to_xs_map[cell->material_id]];
+    size_t i = cell->id * n_groups;
 
-    const int src_id = matid_to_src_map[cell->material_id];
-    if (src_id < 0)
-      continue;
-    const auto src = material_src[src_id];
+    // Inhomogeneous source
+    int src_id = matid_to_src_map[cell->material_id];
+    if (src_id >= 0)
+    {
+      const auto& src = material_src[src_id];
+      for (size_t g = 0; g < n_groups; ++g)
+        b[i + g] += src->values[g] * volume;
+    }
 
-    // Contribute group-wise inhomogeneous source
-    for (size_t g = 0; g < n_groups; ++g)
-      system_rhs[i + g] += src->values[g] * volume;
+    // ================================================== Group transfers
+    if (solution_method == SolutionMethod::ITERATIVE)
+    {
+      for (size_t g = 0; g < n_groups; ++g)
+      {
+        // Scattering term
+        for (size_t gp = 0; gp < n_groups; ++gp)
+          b[i + g] += xs->transfer_matrices[0][g][gp] *
+                    phi[i + gp] * volume;
 
-    // Assemble group-wise boundary terms
+        // Fission term
+        if (xs->is_fissile)
+        {
+          // Total fission
+          if (not use_precursors)
+          {
+            double chi = xs->chi[g];
+            for (size_t gp = 0; gp < n_groups; ++gp)
+              b[i + g] += chi * xs->nu_sigma_f[gp] *
+                        phi[i + gp] * volume;
+
+            for (size_t j = 0; j < xs->n_precursors; ++j)
+            {
+              double chi_d = xs->chi_delayed[g][j];
+              double gamma = xs->precursor_yield[j];
+              for (size_t gp = 0; gp < n_groups; ++gp)
+                b[i + g] += chi_d * gamma *
+                          xs->nu_delayed_sigma_f[gp] *
+                          phi[i + gp] * volume;
+            }
+          }
+        }//if fissile
+      }//for group
+    }//if including group transfers
+
+    //================================================== Loop over faces
     for (const auto& face : cell->faces)
     {
-      // Only consider boundary faces
+      //================================================== Boundary faces
       if (not face.has_neighbor)
       {
-        const size_t bndry_id = face.neighbor_id;
+        const auto bndry_id = face.neighbor_id;
         const auto bndry_type = boundary_info[bndry_id].first;
 
-        // Handle Dirichlet boundaries
+        // Dirichlet boundary term
         if (bndry_type == BoundaryType::DIRICHLET)
         {
           double d_pf = cell->centroid.distance(face.centroid);
-
-          const auto& xs = material_xs[matid_to_xs_map[cell->material_id]];
-          const auto& D = xs->diffusion_coeff;
-
           for (size_t g = 0; g < n_groups; ++g)
           {
             const auto& bndry = boundaries[bndry_id][g];
             const auto bc = std::static_pointer_cast<DirichletBoundary>(bndry);
-            system_rhs[i + g] += D[g]/d_pf * bc->value * face.area;
-          }
-        }//if Dirichlet
 
-        // Handle Neumann boundaries
+            double D = xs->diffusion_coeff[g];
+            b[i + g] += D / d_pf * bc->value * face.area;
+          }
+        }
+
+        // Neumann boundary term
         else if (bndry_type == BoundaryType::NEUMANN)
         {
           for (size_t g = 0; g < n_groups; ++g)
           {
             const auto& bndry = boundaries[bndry_id][g];
             const auto bc = std::static_pointer_cast<NeumannBoundary>(bndry);
-            system_rhs[i + g] += bc->value * face.area;
+            b[i + g] += bc->value * face.area;
           }
-        }//if Neumann
+        }
 
-        // Handle Robin boundaries
+        // Robin boundary term
         else if (bndry_type == BoundaryType::MARSHAK or
                  bndry_type == BoundaryType::ROBIN)
         {
           double d_pf = cell->centroid.distance(face.centroid);
-
-          const auto& xs = material_xs[matid_to_xs_map[cell->material_id]];
-          const auto& D = xs->diffusion_coeff;
-
           for (size_t g = 0; g < n_groups; ++g)
           {
             const auto& bndry = boundaries[bndry_id][g];
             const auto bc = std::static_pointer_cast<RobinBoundary>(bndry);
-            system_rhs[i + g] +=
-                D[g]/(bc->b*D[g] + bc->a*d_pf) * bc->f * face.area;
+
+            double D = xs->diffusion_coeff[g];
+            b[i + g] += D/(bc->b*D + bc->a*d_pf) * bc->f * face.area;
           }
-        }//if Robin
-      }
+        }
+      }//if boundary face
     }//for face
   }//for cell
 }
