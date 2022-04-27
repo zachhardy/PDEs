fa8f3357d278903c90d4c2cdc9c6296e91eca01d
Zachary Hardy <zhardy1115@gmail.com>
2022-04-26 22:40:00 -0600

    Updated run file.

--- PDEs/main.cc
+++ PDEs/main.cc
@@ -1,5 +1,4 @@
 #include "grid_structs.h"
-#include "Discretization/FiniteVolume/finite_volume.h"
 
 #include "material.h"
 #include "CrossSections/cross_sections.h"
@@ -7,7 +6,8 @@
 #include "math.h"
 #include "linear_solver.h"
 
-#include "NeutronDiffusion/SteadyStateSolver/steadystate_solver.h"
+#include "NeutronDiffusion/Groupset/groupset.h"
+#include "NeutronDiffusion/SteadyStateSolver/FV/steadystate_solver_fv.h"
 
 #include <iostream>
 #include <vector>
@@ -24,10 +24,8 @@ int main(int argc, char** argv)
     using namespace physics;
     using namespace neutron_diffusion;
 
-    // Create the solver
-    SteadyStateSolver solver;
 
-    // Create the vertices
+    //================================================== Create the mesh
     size_t n_cells = 100;
     double slab_width = 1.0;
     double cell_width = slab_width / n_cells;
@@ -37,31 +35,45 @@ int main(int argc, char** argv)
       vertices.emplace_back(vertices.back() + cell_width);
 
     auto mesh = create_1d_mesh(vertices, CoordinateSystem::CARTESIAN);
-    solver.mesh = mesh;
 
-    auto discretization = std::make_shared<FiniteVolume>(mesh);
-    solver.discretization = discretization;
-
+    //================================================== Create the materials
     auto material = std::make_shared<Material>();
 
+    // Create the cross sections
     auto xs = std::make_shared<CrossSections>();
-    xs->read_xs_file("xs_data/test_2g.xs");
+    xs->read_xs_file("xs_data/test_1g.xs");
     material->properties.emplace_back(xs);
 
-    std::vector<double> mg_source(xs->n_groups, 1.0);
+    size_t n_groups = xs->n_groups;
+
+    // Create the multigroup source
+    std::vector<double> mg_source(n_groups, 1.0);
     auto src = std::make_shared<IsotropicMultiGroupSource>(mg_source);
     material->properties.emplace_back(src);
 
+    //================================================== Create the solver
+    SteadyStateSolver_FV solver;
+    solver.mesh = mesh;
     solver.materials.emplace_back(material);
 
+    // Initialize groups
+    for (size_t g = 0; g < n_groups; ++g)
+      solver.groups.emplace_back(g);
+
+    // Initialize groupsets
+    Groupset groupset(0);
+    for (size_t g = 0; g < n_groups; ++g)
+      groupset.groups.emplace_back(solver.groups[g]);
+    solver.groupsets.emplace_back(groupset);
+
+    // Define boundary conditions
     solver.boundary_info.emplace_back(BoundaryType::ZERO_FLUX, -1);
     solver.boundary_info.emplace_back(BoundaryType::ZERO_FLUX, -1);
 
-    solver.solution_method = SolutionMethod::ITERATIVE;
-    solver.linear_solver_type = LinearSolverType::LU;
+    solver.options.solution_technique = SolutionTechnique::GROUPSET_WISE;
 
 
-    // Run the problem
+    //================================================== Run the problem
     solver.initialize();
     solver.execute();
   }
