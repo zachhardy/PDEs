a519f143ba0476f804d7bd8bdbf95ecdc13aca5e
Zachary Hardy <zhardy1115@gmail.com>
2022-04-28 10:34:40 -0600

    Added sparsity pattern routine.

---
+++ PDEs/Math/Discretization/FiniteVolume/fv.h
@@ -0,0 +1,67 @@
+#ifndef FV_H
+#define FV_H
+
+#include "Discretization/discretization.h"
+#include "Grid/cell.h"
+
+namespace math
+{
+
+/**
+ * \brief A class for finite volume (FV) discretizations.
+ *
+ * Finite volume discretizations use cell-averaged unknowns, colloquially
+ * located at the cell centers. This method is generally derived by assuming
+ * this form of the solution, plugging it into the governing law, and
+ * integrating over each Cell of the Mesh. This method primarily relies on
+ * geometric information contained within the Cell and Face objects and does
+ * not require many additional routines.
+ */
+class FiniteVolume : public Discretization
+{
+public:   /*---------- Constructors, Destructors, and Assignment ----------*/
+
+  /// Default constructor.
+  explicit FiniteVolume(std::shared_ptr<grid::Mesh> reference_mesh)
+      : Discretization(reference_mesh, DiscretizationMethod::FINITE_VOLUME)
+  {}
+
+public: /*---------- Routines ----------*/
+
+  /** Return the number of nodes per cell.
+   *  For FV discretizations, there is 1 node per cell. */
+  size_t nodes_per_cell() const override
+  { return 1; }
+
+  /** Return the number of DoFs per cell.
+   *  This returns the number of nodes per cell multiplied by the number of
+   *  solution components. For FV discretizations, this returns the number of
+   *  solution components. See \ref nodes_per_cell. */
+  size_t dofs_per_cell(const size_t n_components) const override
+  { return n_components * nodes_per_cell(); }
+
+  /** Return the number of nodes in the discretization.
+   * For FV discretizations, this is equivalent to the number of cells. */
+  size_t n_nodes() const override
+  { return mesh->cells.size(); }
+
+  /** Return the number of DoFs in the discretization.
+   *  This returns the number of nodes multiplied by the number of solution
+   *  components. For FV discretizations, this returns the number of cells times
+   *  the number of solution components. See \ref n_nodes.*/
+  size_t n_dofs(const size_t n_components) const override
+  { return n_components * n_nodes(); }
+
+  /** Return the location of the nodes on the specified Cell. For FV
+   *  discretizations, this returns the Cell centroid. */
+  std::vector<grid::Point> nodes(const grid::Cell& cell) const override
+  { return std::vector<grid::Point>(1, cell.centroid); }
+
+  void
+  make_sparsity_pattern(std::vector<size_t> prealloc,
+                        const size_t n_components = 1,
+                        const bool is_coupled = false) const override;
+};
+
+}
+#endif //FV_H
---
+++ PDEs/Math/Discretization/FiniteVolume/make_sparsity.cc
@@ -0,0 +1,44 @@
+#include "fv.h"
+
+/**
+ * Define the sparsity pattern.
+ *
+ * This routine defines the number of non-zero entries per row for a problem
+ * with the specified number of components. If the \p is_coupled flag is set
+ * to \p true, it is assumed that all components are coupled to one another,
+ * otherwise, it is assumed that the system is uncoupled in all components.
+ * For FV discretizations, each node is coupled to those on the neighboring
+ * cells.
+ *
+ * \param prealloc The number of entries per row to preallocate.
+ * \param n_components The number of components in the solution.
+ * \param is_coupled A flag for allocating storage for coupling between
+ *   solution components.
+ */
+void math::FiniteVolume::
+make_sparsity_pattern(std::vector<size_t> prealloc,
+                      const size_t n_components, const bool is_coupled) const
+{
+  // Resive based on the number of DoFs
+  sparsity_pattern.resize(this->n_dofs(n_components));
+
+  // Loop over cells
+  for (const auto& cell : mesh->cells)
+  {
+    const size_t ir = cell->id * n_components;
+
+    for (size_t c = 0; c < n_components; ++c)
+      sparsity_pattern[ir + c] += is_coupled ? n_components : 1;
+
+    // Loop over faces
+    for (const auto& face : cell->faces)
+    {
+      if (face.has_neighbor)
+      {
+        const size_t jr = face.neighbor_id * n_components;
+        for (size_t c = 0; c < n_components; ++c)
+          sparsity_pattern[ir + c] += 1;
+      }
+    }//for face
+  }//for cells
+}
