2be7985758d67625bb473aaf2ab58d0766b71a90
Zachary Hardy <zhardy1115@gmail.com>
2022-05-04 15:48:37 -0600

    Added base sparse linear solver class.

---
+++ PDEs/Math/LinearSolvers/Iterative/jacobi.h
@@ -0,0 +1,4 @@
+#ifndef JACOBI_H
+#define JACOBI_H
+
+#endif //JACOBI_H
--- PDEs/Math/LinearSolvers/linear_solver.h
+++ PDEs/Math/LinearSolvers/linear_solver.h
@@ -1,8 +1,9 @@
 #ifndef LINEAR_SOLVER_H
 #define LINEAR_SOLVER_H
 
-#include "Math/matrix.h"
-#include "Math/vector.h"
+#include "matrix.h"
+#include "sparse_matrix.h"
+#include "vector.h"
 
 namespace math
 {
@@ -39,6 +40,46 @@ public:
   }
 
   void set_matrix(Matrix<value_type>& matrix)
+  {
+    A = matrix;
+    initialized = false;
+  }
+
+public:
+  /** Abstract setup method. */
+  virtual void setup() = 0;
+
+  /** Abstract solve method. */
+  virtual Vector<value_type> solve(const Vector<value_type>& b) = 0;
+};
+
+
+//######################################################################
+/**
+ * An abstract base class for solving the linear system
+ * \f$ \boldsymbol{A} \vec{x} = \vec{b} \f$.
+ */
+template<typename value_type>
+class SparseLinearSolver
+{
+protected:
+  bool initialized = false;
+  SparseMatrix<value_type>& A;
+
+public:
+  SparseLinearSolver(SparseMatrix<value_type>& matrix) : A(matrix)
+  {
+    if (A.n_rows() != A.n_cols())
+    {
+      std::stringstream err;
+      err << "LinearSystem::" << __FUNCTION__ << ": "
+          << "Invalid inputs. The matrix must be square and of the same "
+          << "dimension as the right-hand side vector.";
+      throw std::runtime_error(err.str());
+    }
+  }
+
+  void set_matrix(SparseMatrix<value_type>& matrix)
   {
     A = matrix;
     initialized = false;
