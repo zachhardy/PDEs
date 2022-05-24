dcc630b2853693fc295758829832730956baebd4
Zachary Hardy <zhardy1115@gmail.com>
2022-05-24 17:33:51 -0600

    Implemented Gaussian elimination solver.

---
+++ PDEs/Math/LinearSolvers/Direct/gaussian_elimination.cc
@@ -0,0 +1,158 @@
+#include "gaussian_elimination.h"
+#include "macros.h"
+
+#include <cmath>
+
+using namespace pdes;
+
+Math::Vector
+Math::gaussian_elimination(Matrix& A, Vector& b, const bool pivot)
+{
+  Assert(A.n_rows() == A.n_cols(),
+         "Square matrices are required for Gaussian elimination.");
+  Assert(b.size() == A.n_rows(), "Dimension mismatch error.");
+
+  size_t n = b.size();
+
+  //======================================== Row-echelon factorization
+  // Go through the columns of the matrix
+  for (size_t j = 0; j < n; ++j)
+  {
+    /* Find the row index for the largest magnitude entry in this column.
+     * This is only done for sub-diagonal elements. */
+    if (pivot)
+    {
+      size_t argmax = j;
+      double max = A(j, j);
+      for (size_t k = j; k < n; ++k)
+      {
+        const double a_kj = A(k, j);
+        if (std::fabs(a_kj) > max)
+        {
+          argmax = k;
+          max = std::fabs(a_kj);
+        }
+      }
+
+      // If the sub-diagonal is uniformly zero, throw error
+      Assert(max != 0.0, "Singular matrix error.");
+
+      /* Swap the current row and the row containing the largest magnitude
+       * entry corresponding for the current column. This is done to improve
+       * the numerical stability of the algorithm. */
+      if (argmax != j)
+      {
+        std::swap(b[j], b[argmax]);
+        A.swap_row(j, argmax);
+      }
+    }//if pivot
+
+    const double* a_j = A.data(j); // accessor for row j
+    const double a_jj = a_j[j]; // diagonal element for row j
+
+    /* Perform row-wise operations such that all sub-diagonal values are zero.
+     * This is done by subtracting the current row times the ratio of the
+     * sub-diagonal and the current row's leading value. */
+    for (size_t i = j + 1; i < n; ++i)
+    {
+      double* a_i = A.data(i); // accessor for row i
+
+      const double factor = a_i[j] / a_jj;
+      for (size_t k = j; k < n; ++k)
+        a_i[k] -= a_j[k] * factor;
+      b[i] -= b[j] * factor;
+    }
+  }
+
+  //======================================== Back substitution solve
+  Vector x(n, 0.0);
+  for (size_t i = n - 1; i != -1; --i)
+  {
+    const double* a_i = A.data(i); // accessor for row i
+    const double a_ii = a_i[i]; // diagonal element for row i
+    a_i += i + 1; // increment to first element after diagonal
+
+    double value = b[i];
+    for (size_t j = i + 1; j < n; ++j)
+      value -= *a_i++ * x[j];
+    x[i] = value / a_ii;
+  }
+  return x;
+}
+
+
+Math::Vector
+Math::gaussian_elimination(SparseMatrix& A, Vector& b, const bool pivot)
+{
+  Assert(A.n_rows() == A.n_cols(),
+         "Square matrices are required for Gaussian elimination.");
+  Assert(b.size() == A.n_rows(), "Dimension mismatch error.");
+
+  size_t n = b.size();
+
+  //======================================== Row-echelon factorization
+  // Go through the columns of the matrix
+  for (size_t j = 0; j < n; ++j)
+  {
+    /* Find the row index for the largest magnitude entry in this column.
+     * This is only done for sub-diagonal elements. */
+    if (pivot)
+    {
+      const double* a_jj = A.diagonal(j);
+
+      size_t argmax = j;
+      double max = std::fabs((a_jj)? *a_jj : 0.0);
+      for (size_t k = j + 1; k < n; ++k)
+      {
+        const double* a_kj = A.locate(k, j);
+        if (a_kj && *a_kj > max)
+        {
+          argmax = k;
+          max = std::fabs(*a_kj);
+        }
+      }
+
+      // If the sub-diagonal is uniformly zero, throw an error
+      Assert(max != 0.0, "Singular matrix error.");
+
+      /* Swap the current row and the row containing the largest magnitude
+       * entry corresponding for the current column. This is done to improve
+       * the numerical stability of the algorithm. */
+      if (argmax != j)
+      {
+        std::swap(b[j], b[argmax]);
+        A.swap_row(j, argmax);
+      }
+    }//if pivot
+
+    const double a_jj = *A.diagonal(j);
+
+    /* Perform row-wise operations such that all sub-diagonal values are zero.
+     * This is done by subtracting the current row times the ratio of the
+     * sub-diagonal and the current row's leading value. */
+    for (size_t i = j + 1; i < n; ++i)
+    {
+      const double* a_ij = A.locate(i, j);
+      if (a_ij && *a_ij != 0.0)
+      {
+        const double factor = *a_ij /a_jj;
+        for (const auto el : A.const_row_iterator(j))
+          if (el.column >= j)
+            A.add(i, el.column, -el.value * factor);
+        b[i] -= b[j] * factor;
+      }
+    }
+  }
+
+  //======================================== Back substitution solve
+  Vector x(n, 0.0);
+  for (size_t i = n - 1; i != -1; --i)
+  {
+    double value = b[i];
+    for (const auto el : A.const_row_iterator(i))
+      if (el.column > i)
+        value -= el.value * x[el.column];
+    x[i] = value / *A.diagonal(i);
+  }
+  return x;
+}
---
+++ PDEs/Math/LinearSolvers/Direct/gaussian_elimination.h
@@ -0,0 +1,36 @@
+#ifndef GAUSSIAN_ELIMINATION_H
+#define GAUSSIAN_ELIMINATION_H
+
+#include "matrix.h"
+#include "sparse_matrix.h"
+#include "vector.h"
+
+
+namespace pdes::Math
+{
+
+/**
+ * Solve a system using Gaussian elimination.
+ *
+ * This routine factors the matrix and right-hand side into row-echelon form,
+ * then uses back substitution to directly obtain the solution. Row-echelon
+ * form creates an upper triangular system. This is done by traversing through
+ * each column and performing row-wise operations to eliminate all sub-diagonal
+ * coefficients. For numerical stability, pivoting can be used to ensure that
+ * for each subsequent column, division by the largest element is carried out.
+ *
+ * \param A An \f$ n \times n \f$ matrix.
+ * \param b A vector of length \f$ n \f$.
+ * \param pivot A flag for whether pivoting is performed.
+ * \return The solution \f$ \vec{x} \f$ of
+ *         \f$ \boldsymbol{A} \vec{x} = \vec{b} \f$.
+ */
+Vector
+gaussian_elimination(Matrix& A, Vector& b, const bool pivot = true);
+
+
+Vector
+gaussian_elimination(SparseMatrix& A, Vector& b, const bool pivot = true);
+
+}
+#endif //GAUSSIAN_ELIMINATION_H
