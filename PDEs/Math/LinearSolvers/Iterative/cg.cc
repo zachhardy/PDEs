fc7486afd2698c936d1f971d8f48d0fd328b44fc
Zachary Hardy <zhardy1115@gmail.com>
2022-06-06 09:11:18 -0600

    Auto stash before rebase of "refs/heads/development"

--- PDEs/Math/LinearSolvers/Iterative/cg.cc
+++ PDEs/Math/LinearSolvers/Iterative/cg.cc
@@ -4,6 +4,7 @@
 #include "Sparse/sparse_matrix.h"
 
 #include "macros.h"
+#include "Timer/timer.h"
 
 #include <cmath>
 
@@ -22,7 +23,9 @@ solve(Vector& x, const Vector& b) const
 {
   size_t n = A->n_rows();
   Assert(b.size() == n, "Dimension mismatch error.")
-  Assert(x.size() == n, "Dimension mismatrch error.")
+  Assert(x.size() == n, "Dimension mismatch error.")
+
+  double norm = b.l2_norm();
 
   // Allocate data needed for the CG solver
   Vector r(x.size());
@@ -44,27 +47,63 @@ solve(Vector& x, const Vector& b) const
 
   //======================================== Iteration loop
   size_t nit;
+  Timer timer;
+  std::vector<double> times;
   for (nit = 0; nit < max_iterations; ++nit)
   {
+    times.clear();
+
     // Precompute necessary matrix-vector product q = Ap
+    timer.start();
     A->vmult(p, q);
+    timer.stop();
+    times.push_back(timer.get_time());
 
     // Recompute alpha factor
+    timer.start();
     alpha = res_prev / p.dot(q);
+    timer.stop();
+    times.push_back(timer.get_time());
 
     // Update solution and residual vector
+    timer.start();
     x.add(p, alpha);
+    timer.stop();
+    times.push_back(timer.get_time());
+
+    timer.start();
     r.add(q, -alpha);
+    timer.stop();
+    times.push_back(timer.get_time());
 
     // Update residual norm
+    timer.start();
     res = r.dot(r);
+    timer.stop();
+    times.push_back(timer.get_time());
 
     // Check convergence
-    if (check(nit + 1, std::sqrt(res)))
+    timer.start();
+    bool converged = check(nit + 1, std::sqrt(res));
+    timer.stop();
+    times.push_back(timer.get_time());
+
+    if (converged)
       break;
 
     // If not converged, prep for next iteration
+    timer.start();
     p.sadd(res / res_prev, r);
+    timer.stop();
+    times.push_back(timer.get_time());
+
+    timer.start();
     res_prev = res;
+    timer.stop();
+    times.push_back(timer.get_time());
+
+    double total_time = 0.0;
+    for (const auto& time : times)
+      total_time += time;
   }
 }
--- PDEs/main.cc
+++ PDEs/main.cc
@@ -4,6 +4,10 @@
 #include "material.h"
 #include "CrossSections/cross_sections.h"
 
+#include "LinearSolvers/DirectSolvers"
+#include "LinearSolvers/IterativeSolvers"
+#include "LinearSolvers/PETSc/petsc_solver.h"
+
 #include "NeutronDiffusion/Groupset/groupset.h"
 #include "NeutronDiffusion/SteadyStateSolver/steadystate_solver.h"
 #include "NeutronDiffusion/KEigenvalueSolver/keigenvalue_solver.h"
@@ -23,27 +27,33 @@ int main(int argc, char** argv)
 {
   try{
     using namespace Grid;
+
     using namespace Math;
+    using namespace Math::LinearSolver;
+
     using namespace Physics;
+
     using namespace NeutronDiffusion;
 
-    //================================================== Create the mesh
-    size_t n_cells = 200;
-    double slab_width = 6.0;
+    //================================================== Mesh
+
+    size_t n_cells = 500;
+    double slab_width = 1.0;
     double cell_width = slab_width / n_cells;
 
     std::vector<double> vertices(1, 0.0);
     for (size_t i = 0; i < n_cells; ++i)
       vertices.emplace_back(vertices.back() + cell_width);
 
-    auto mesh = create_1d_mesh(vertices, CoordinateSystem::SPHERICAL);
+    auto mesh = create_1d_mesh(vertices, CoordinateSystem::CARTESIAN);
 
-    //================================================== Create the materials
+    //================================================== Materials
+
     auto material = std::make_shared<Material>();
 
     // Create the cross sections
     auto xs = std::make_shared<CrossSections>();
-    xs->read_xs_file("xs_data/test_3g.xs");
+    xs->read_xs_file("xs_data/test_1g.xs");
     material->properties.emplace_back(xs);
 
     size_t n_groups = xs->n_groups;
@@ -53,10 +63,23 @@ int main(int argc, char** argv)
     auto src = std::make_shared<IsotropicMultiGroupSource>(mg_source);
     material->properties.emplace_back(src);
 
+    //================================================== Linear Solver
+
+    Options opts;
+    opts.verbosity = 1;
+    opts.tolerance = 1.0e-6;
+
+//    auto linear_solver = std::make_shared<PETScSolver>(KSPCG, PCNONE, opts);
+    auto linear_solver = std::make_shared<CG>(opts);
+
     //================================================== Create the solver
-    KEigenvalueSolver solver;
+    SteadyStateSolver solver;
     solver.mesh = mesh;
     solver.materials.emplace_back(material);
+    solver.linear_solver = linear_solver;
+
+    solver.verbose = true;
+    solver.use_precursors = false;
 
     // Initialize groups
     for (size_t g = 0; g < n_groups; ++g)
@@ -69,26 +92,20 @@ int main(int argc, char** argv)
     solver.groupsets.emplace_back(groupset);
 
     // Define boundary conditions
-    solver.boundary_info.emplace_back(BoundaryType::REFLECTIVE, -1);
+    solver.boundary_info.emplace_back(BoundaryType::ZERO_FLUX, -1);
     solver.boundary_info.emplace_back(BoundaryType::ZERO_FLUX, -1);
 
-    solver.solution_technique = SolutionTechnique::FULL_SYSTEM;
-    solver.linear_solver_type = LinearSolver::LinearSolverType::SSOR;
+    solver.solution_technique = SolutionTechnique::GROUPSET_WISE;
 
-    solver.linear_solver_opts.verbose_history = true;
-    solver.linear_solver_opts.verbose_result = false;
-
-    solver.use_precursors = false;
-    solver.verbose = false;
 
     //================================================== Run the problem
 
-//    PetscErrorCode ierr = PetscInitialize(&argc,&argv,(char*)0,NULL);
+    PetscInitialize(&argc,&argv,(char*)0,NULL);
 
     solver.initialize();
     solver.execute();
 
-//    PetscFinalize();
+    PetscFinalize();
   }
   catch (std::exception &exc) {
     std::cerr << std::endl
